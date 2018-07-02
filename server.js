d5f966739afa8b144b5f885cf69a9613d4cf5b82
John Erickson <ericksonjohn08@hotmail.com>
2018-07-01 21:43:51 -0400

    Add get function to /messages path passing in dummy data. Add getMessages function populating messages automatically.

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var app = express();
 
+// MIDDLEWEAR
 app.use(express.static(__dirname + '/public'));
-// Serve static content for the app from the “public” directory in the application directory:
+// Serve static content for the app from the “public” directory in the application directory
+
+let messages = [
+  {name: 'John', message: 'Hello John'},
+  {name: 'Lena', message: 'Hello Lena'}
+];
+
+app.get('/messages', (req, res) => {
+  res.send(messages);
+});
 
 let server = app.listen(3000, () => {
   console.log('App Chat is listening on port 3000!'), server.address().port
